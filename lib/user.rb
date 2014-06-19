e60b6bd1ecb0ed1e56bbf776ec33a976b0342605
Sasha Cooper <jinksy@gmail.com>
2014-06-19 12:38:59 +0100

    prevented duplicate email addresses

--- lib/user.rb
+++ lib/user.rb
@@ -6,15 +6,16 @@ class User
 
 	attr_reader :password
 	attr_accessor :password_confirmation
-	validates_confirmation_of :password
+	validates_confirmation_of :password, message: "Sorry, your passwords don't match"
 
 	property :id, Serial
-	property :email, String
+	property :email, String, unique: true, message: "This email is already taken"
 	# this will store both the password and the salt
   # It's Text and not String because String holds 
   # 50 characters by default
   # and it's not enough for the hash and salt
 	property :password_digest, Text
+	# property :email, String, unique: true
 
 	def password=(password)
 		@password = password
--- server.rb
+++ server.rb
@@ -60,7 +60,7 @@ post '/users' do
 		session[:user_id] = @user.id
 		redirect to('/')
 	else
-		flash.now[:notice] = "Sorry, your passwords don't match"
+		flash.now[:errors] = @user.errors.full_messages
 		erb :"users/new"
 	end
 end
