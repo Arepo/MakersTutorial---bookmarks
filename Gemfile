cfb058fcd11d1a002dcdb123d7ef30f68d58ddf7
Sasha Cooper <jinksy@gmail.com>
2014-06-20 08:15:23 +0100

    Extracted actions from Server.rb into separate controllers

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt-ruby'
 gem 'sinatra-flash'
+gem 'sinatra-partial'
 
 group :development, :test do
 	gem "rspec"
--- app/server.rb
+++ app/server.rb
@@ -6,77 +6,16 @@ require './lib/tag'
 require './lib/user'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
+require_relative 'controllers/links'
+require_relative 'controllers/sessions'
+require_relative 'controllers/tags'
+require_relative 'controllers/users'
 
 enable :sessions
 set :session_secret, 'super secret'
 
-get '/' do
-	@links = Link.all
-	erb :index
-end
 
-post '/links' do
-	url = params["url"]
-	title = params["title"]
-	tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
-	Link.create(url: url, title: title, tags: tags)
-	redirect to('/')
-end
 
-get '/tags/:text' do
-	tag = Tag.first(text: params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
 
-get '/users/new' do
-	@user = User.new
-	erb :"users/new"
-	# note the view is in views/users/new.erb
-  # we need the quotes because otherwise
-  # ruby would divide the symbol :users by the
-  # variable new (which makes no sense)
-end
 
-post '/users' do
-	@user = User.new(email: params[:email],
-				password: params[:password],
-				password_confirmation: params[:password_confirmation])
-	# we just initialize the object
-  # without saving it. It may be invalid
-	if @user.save
-		session[:user_id] = @user.id
-		redirect to('/')
-		  # let's try saving it
-  # if the model is valid,
-  # it will be saved
-	else
-		flash.now[:errors] = @user.errors.full_messages
-		erb :"users/new"
-		 # if it's not valid,
-    # we'll show the same
-    # form again
-	end
-end
-
-get '/sessions/new' do
-	erb :"sessions/new"
-end
-
-post '/sessions' do
-	email, password = params[:email], params[:password]
-	user = User.authenticate(email, password)
-	if user
-		session[:user_id] = user.id
-		redirect to('/')
-	else
-		flash[:errors] = ["The email or password is incorrect"]
-		erb :"sessions/new"
-	end
-end
-
-delete '/sessions' do
-	session[:user_id] = nil
-	flash.now[:notice] = "Good bye!"
-end
 
